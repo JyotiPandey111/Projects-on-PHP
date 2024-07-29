6ff09fc5780ae9812a0d38f585c09a2b1fbf8629
JyotiPandey111 <jyotipandeydip@gmail.com>
2024-07-29 11:45:33 +0530

    using include in php

---
+++ 01-FIRST-STEPS/03-include/other.php
@@ -0,0 +1,1 @@
+<h1>This is the Content from other.php file</h1>
---
+++ 01-FIRST-STEPS/03-include/start.php
@@ -0,0 +1,3 @@
+<h1>This is content from start file</h1>
+<?php include 'other.php'; ?>
+<h2> Random number generated from other.txt: <?php include 'other.txt'; ?></h2>
