9b0a15f1c57f02e8516a0c962ef6e01b0679b342
JyotiPandey111 <jyotipandeydip@gmail.com>
2024-07-29 11:44:51 +0530

    integrating php with html and using random number

---
+++ 01-FIRST-STEPS/01-first-steps.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Hello from Jyoti Pandey!'; ?></h1>
+    <h1>Hey, This is my first .php file. </h1>
+    <h2>Integrating php with html</h2>
+    <h3>php rand number using rand function: <?php echo rand(1,6);?></h3>
+</body>
+</html>
